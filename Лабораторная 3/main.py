f41442fd32da578f2bad24559ee011fff6af6094
korobedu <115357300+korobedu@users.noreply.github.com>
2022-12-26 00:03:21 +0300

    Лабораторная работа 3

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -1,31 +1,71 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Книга {self.name}. Автор {self.author}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
+    @property
+    def name(self) -> str:
+        return self._name
 
-class PaperBook:
+    @name.setter
+    def name(self, name_: str):
+        self._name = name_
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    @author.setter
+    def author(self, author_: str):
+        self._author = author_
+
+
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+        super().__init__(name, author)
+        self._pages = pages
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, " \
+               f"pages={self.pages!r})"
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages_: int):
+        if 0 > pages_:
+            raise ValueError('pages < 0')
+        if not isinstance(pages_, int):
+            raise TypeError("pages isn't int")
+        self._pages = pages_
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+        super().__init__(name, author)
+        self._duration = duration
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, " \
+               f"pages={self.duration!r})"
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration_: float):
+        if duration_ < 0:
+            raise ValueError("duration < 0")
+        if not isinstance(duration_, float):
+            raise TypeError("duration isn't float")
+        self._duration = duration_
